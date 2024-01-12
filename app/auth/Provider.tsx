4c4b1bc6dfd2dad97ffe417836e585aeec6a3242
MasonGarrett <thegarretts99@gmail.com>
2024-01-12 13:36:12 -0500

    Add the login and logout links

--- app/auth/Provider.tsx
+++ app/auth/Provider.tsx
@@ -3,6 +3,7 @@
 import { SessionProvider } from 'next-auth/react';
 import { PropsWithChildren } from 'react';
 
+// Used for checking user status
 const AuthProvider = ({ children }: PropsWithChildren) => {
     return <SessionProvider>{children}</SessionProvider>;
 };
