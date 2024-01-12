c7c1a3a74afe51f2dbd8e5017779f62a10952edb
MasonGarrett <thegarretts99@gmail.com>
2024-01-12 10:40:38 -0500

    Improve the loading experience

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,4 +1,10 @@
-import IssueForm from '../_components/IssueForm';
+// Used to dynamically load component
+import dynamic from 'next/dynamic';
+import IssueFormSkeleton from './loading';
+const IssueForm = dynamic(() => import('@/app/issues/_components/IssueForm'), {
+    ssr: false,
+    loading: () => <IssueFormSkeleton />,
+});
 
 const NewIssuePage = () => {
     return <IssueForm />;
