a868aa7da13b8fac4e93e3c21afb617bf7249b52
MasonGarrett <thegarretts99@gmail.com>
2024-01-12 14:10:21 -0500

    Add a loading skeleton

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { Skeleton } from '@/app/components';
 import {
     Avatar,
     Box,
@@ -67,7 +68,7 @@ const NavLinks = () => {
 const AuthStatus = () => {
     const { status, data: session } = useSession();
 
-    if (status === 'loading') return null;
+    if (status === 'loading') return <Skeleton width="3rem" />;
 
     if (status === 'unauthenticated')
         return (
