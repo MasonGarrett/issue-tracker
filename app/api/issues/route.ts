40c3f3adb800a913d9a683014b0c3b5ffe334b7a
MasonGarrett <thegarretts99@gmail.com>
2024-01-15 09:17:51 -0500

    Filter Issues

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,4 +1,5 @@
 import authOptions from '@/app/auth/authOptions';
+import prisma from '@/prisma/client';
 import { getServerSession } from 'next-auth';
 import { NextRequest, NextResponse } from 'next/server';
 import { issueSchema } from '../../validationSchemas';
@@ -13,4 +14,10 @@ export async function POST(request: NextRequest) {
 
     if (!validation.success)
         return NextResponse.json(validation.error.format(), { status: 400 });
+
+    const newIssue = await prisma.issue.create({
+        data: { title: body.title, description: body.description },
+    });
+
+    return NextResponse.json(newIssue, { status: 201 });
 }
