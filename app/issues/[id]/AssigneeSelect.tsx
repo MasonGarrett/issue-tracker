40ea64d0a631ac25fa949db34a76e713302322d1
MasonGarrett <thegarretts99@gmail.com>
2024-01-15 08:51:17 -0500

    Refactor AssigneeSelect component

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -7,33 +7,26 @@ import axios from 'axios';
 import toast, { Toaster } from 'react-hot-toast';
 
 const AssigneeSelect = ({ issue }: { issue: Issue }) => {
-    const {
-        data: users,
-        error,
-        isLoading,
-    } = useQuery<User[]>({
-        queryKey: ['users'],
-        queryFn: () =>
-            axios.get('/api/users').then((response) => response.data),
-        staleTime: 60 * 1000, // 60s
-        retry: 3,
-    });
+    const { data: users, error, isLoading } = useUsers();
+
     if (isLoading) return <Skeleton />;
     if (error) return null;
+
+    const assignIssue = (userId: string) => {
+        axios
+            .patch('/api/issues/' + issue.id, {
+                assignedToUserId: userId || null,
+            })
+            .catch(() => {
+                toast.error('Changes could not be saved.');
+            });
+    };
 
     return (
         <>
             <Select.Root
                 defaultValue={issue.assignedToUserId || ''}
-                onValueChange={(userId) => {
-                    axios
-                        .patch('/api/issues/' + issue.id, {
-                            assignedToUserId: userId || null,
-                        })
-                        .catch(() => {
-                            toast.error('Changes could not be saved.');
-                        });
-                }}
+                onValueChange={assignIssue}
             >
                 <Select.Trigger placeholder="Assign..." />
                 <Select.Content>
@@ -52,5 +45,14 @@ const AssigneeSelect = ({ issue }: { issue: Issue }) => {
         </>
     );
 };
+
+const useUsers = () =>
+    useQuery<User[]>({
+        queryKey: ['users'],
+        queryFn: () =>
+            axios.get('/api/users').then((response) => response.data),
+        staleTime: 60 * 1000, // 60s
+        retry: 3,
+    });
 
 export default AssigneeSelect;
