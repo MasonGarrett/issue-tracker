af8a1ce3aeab1e7923e9127b2ad86b5320e207be
MasonGarrett <thegarretts99@gmail.com>
2024-01-15 16:18:09 -0500

    Refactor: HomePage

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,11 @@ import IssueChart from './IssueChart';
 import IssueSummary from './IssueSummary';
 import LatestIssues from './LatestIssues';
 
-export default async function Home({
+const HomePage = async ({
     searchParams,
 }: {
     searchParams: { page: string };
-}) {
+}) => {
     const open = await prisma.issue.count({ where: { status: 'OPEN' } });
     const inProgress = await prisma.issue.count({
         where: { status: 'IN_PROGRESS' },
@@ -32,9 +32,11 @@ export default async function Home({
             <LatestIssues />
         </Grid>
     );
-}
+};
 
 export const metadata: Metadata = {
     title: 'Issue Tracker - Dashboard',
     description: 'View a summary of project issues.',
 };
+
+export default HomePage;
